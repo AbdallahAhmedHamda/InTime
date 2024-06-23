ae32004c8641d2057b62cfe2c05fb874548517ab
AbdallahAhmedHamda <abdallah.ahmed23.2.2003@gmail.com>
2024-06-23 19:52:52 +0300

    notifications

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,11 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { setCurrentPage, removeAllPopups } from '../features/navigation/navigationSlice'
 import { useEffect } from 'react'
+import { format, isToday, isYesterday } from 'date-fns'
+import ActionIcon from '../svg/notifications/ActionIcon'
+import MissedIcon from '../svg/notifications/MissedIcon'
 import '../css/pages/Notifications.css'
 
 export default function Notifications() {
-  const notifications = useSelector((state) => state.user.notifications)
-
+  const notifications = useSelector((state) => state.user.notifications).slice().reverse().slice(0, 30)
 
   const dispatch = useDispatch()
 
@@ -15,15 +17,41 @@ export default function Notifications() {
     dispatch(removeAllPopups())
   }, [dispatch])
 
+  const getNotificationDay = (date) => {
+    if (isToday(date)) {
+        return `today, at ${format(date, 'h:mm a').replace('AM', 'am').replace('PM', 'pm')}`;
+    } else if (isYesterday(date)) {
+        return `yesterday, at ${format(date, 'h:mm a').replace('AM', 'am').replace('PM', 'pm')}`;
+    } else {
+        return `${format(date, "EEE',' 'at' h:mm a").replace('AM', 'am').replace('PM', 'pm')}`;
+    }
+}
+
   return (
     <div className='main-content'>
       <p className='page-name'>Notifications</p>
 
-      {
-        notifications.map((notification) => (
-          <p style={{marginBottom: '10px'}} key={notification._id}>{notification.message}</p>
-        ))
-      }
+      <div className='all-notifications-container'>
+        {
+          notifications.map((notification) => (
+            <div key={notification._id} className='single-notification-container'>
+              {
+                notification.message.includes("time is up") ? (
+                  <MissedIcon />
+                ) : (
+                  <ActionIcon />
+                )
+              }
+
+              {notification.message}
+
+              <p className='single-notifications-date'>
+                {getNotificationDay(notification.date)}
+              </p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
