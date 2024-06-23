6e99dcaf1f8278d2e36778b30792542066f84ff0
AbdallahAhmedHamda <abdallah.ahmed23.2.2003@gmail.com>
2024-06-23 05:47:43 +0300

    notification test

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,9 +1,12 @@
-import { useDispatch, } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setCurrentPage, removeAllPopups } from '../features/navigation/navigationSlice'
 import { useEffect } from 'react'
 import '../css/pages/Notifications.css'
 
 export default function Notifications() {
+  const notifications = useSelector((state) => state.user.notifications)
+
+
   const dispatch = useDispatch()
 
   // change the current page so the app can rerender and update sidenav active icon and reset the unread count to 0 and remove all popups
@@ -15,6 +18,12 @@ export default function Notifications() {
   return (
     <div className='main-content'>
       <p className='page-name'>Notifications</p>
+
+      {
+        notifications.map((notification) => (
+          <p style={{marginBottom: '10px'}} key={notification._id}>{notification.message}</p>
+        ))
+      }
     </div>
   )
 }
